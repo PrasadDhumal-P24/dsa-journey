28ade40bfa7c69ec7058cf2531ec770a3de93a0e
PrasadDhumal-P24 <suvarnadhumal121@gmail.com>
2026-08-27 10:43:27 -0700

    solve reverse linked list problem

---
+++ LinkedList/ReverseLL.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+
+public:
+    int data;
+    node *next;
+};
+
+node *reverse(node *head)
+{
+
+    node *prev = NULL;
+    node *current = head;
+    node *next = current->next;
+
+    while (current != NULL)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+
+    return prev;
+}
+
+int main()
+{
+
+    node *first = new node();
+    node *second = new node();
+    node *third = new node();
+    node *fourth = new node();
+
+    first->data = 10;
+    second->data = 20;
+    third->data = 30;
+    fourth->data = 40;
+
+    first->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = NULL;
+
+    node *newNode = reverse(first);
+
+    node *temp = newNode;
+
+    while (temp != NULL)
+    {
+
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    return 0;
+}
