16a41ca01fe6b272a85813e2e39db5de3f61c61f
PrasadDhumal-P24 <suvarnadhumal121@gmail.com>
2026-08-28 09:41:37 -0700

    practicing a all zeros to end in array problem

--- Array/ZerosToEnd.cpp
+++ Array/ZerosToEnd.cpp
@@ -7,20 +7,24 @@ using namespace std;
 
 int main()
 {
-    int arr[] = {1, 0, 2, 0, 3, 0};
-    int n = 6;
-    int j = 0;
+
+    int arr[] = {5, 0, 6, 0, 77, 89, 0, 99};
+    int n = 8;
+    int pos = 0;
 
     for (int i = 0; i < n; i++)
     {
+
         if (arr[i] != 0)
         {
-            swap(arr[i], arr[j]);
+            swap(arr[i], arr[pos]);
+            pos++;
         }
     }
-    for (int i = 0; i < n; i++)
+
+    for (int pos = 0; pos < n; pos++)
     {
-        cout << arr[i] << " " << endl;
+        cout << arr[pos] << " " << endl;
     }
     return 0;
 }
