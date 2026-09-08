a1d0bf29c1936297f2c23b891cb6c0e69cb32361
PrasadDhumal-P24 <suvarnadhumal121@gmail.com>
2026-09-08 09:50:11 -0700

    solve a new sliding window problem

---
+++ PlacementDSA/ContignousSubArraySum.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int arr[] = {1, 2, 3, 4, 5, 6, 7};
+    int n = 7;
+
+    int start = 0;
+    int end = 0;
+    int sum = 0;
+    int target = 7;
+
+    while (end < n)
+    {
+        sum = sum + arr[end];
+
+        while (sum < target && start <= end)
+        {
+
+            sum = sum - arr[end];
+            start++;
+        }
+
+        if (sum == target)
+        {
+
+            cout << "subarray : ";
+
+            for (int i = start; i <= end; i++)
+            {
+                cout << arr[i] << " ";
+            }
+            cout << endl;
+            break;
+        }
+
+        end++;
+    }
+    return 0;
+}
