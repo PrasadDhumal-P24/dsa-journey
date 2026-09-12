cba18c160b494f1a089b34adfe712290bdc606b1
PrasadDhumal-P24 <suvarnadhumal121@gmail.com>
2026-09-12 10:12:27 -0700

    Detect starting point of Linked List

---
+++ PlacementDSA/StartingPointOfLL.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+};
+
+node *cycleStart(node *head)
+{
+    node *slow = head;
+    node *fast = head;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+        {
+            break;
+        }
+    }
+
+    if (fast == NULL || fast->next == NULL)
+    {
+        return NULL;
+    }
+
+    slow = head;
+
+    while (slow != fast)
+    {
+        slow = slow->next;
+        fast = fast->next;
+    }
+
+    return slow;
+}
+
+int main()
+{
+
+    node *first = new node{10, NULL};
+    node *second = new node{20, NULL};
+    node *third = new node{30, NULL};
+    node *fourth = new node{40, NULL};
+    node *fifth = new node{50, NULL};
+
+    first->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+
+    fifth->next = third;
+
+    node *answer = cycleStart(first);
+
+    if (answer)
+    {
+        cout << "Starting point found cycle = " << answer->data << endl;
+    }
+    else
+    {
+        cout << "starting point not found" << endl;
+    }
+
+    return 0;
+}
